4deaf67594a82bce98a56b9964d71565e98440d7
Eghosa Igbinedion <eghos24@yahoo.com>
2022-09-08 18:54:46 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,10 @@
 /**
  *  This program prints a message using puts funtion
  *  main - main block
- *  Mystr - string to be printed
- *  return: 0
+ *  return:0
 */
 int main(void) 
 {
-		char Mystr[] = "\"Programming is like building a multilingual puzzle";
-
-		puts(Mystr);
+		puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
