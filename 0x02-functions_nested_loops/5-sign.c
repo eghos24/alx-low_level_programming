43c6db62f786cd68afc88e099dc2f8182fb53a52
Eghosa Igbinedion <eghos24@yahoo.com>
2022-09-13 19:59:33 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ * Description: prints if a number is +,-,0
+ *@n: the integer value it recieves
+ * Return: 1 if true, 0 if false.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{	_putchar('+');
+		return (1);
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0')
+		return (0);
+}
