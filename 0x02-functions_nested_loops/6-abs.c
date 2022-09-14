136ede0b26eda3b7406c6b227726feaacc677671
Eghosa Igbinedion <eghos24@yahoo.com>
2022-09-13 20:18:59 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * Description: prints the absolite value of a given number
+ *@n: the integer value it recieves
+ * Return: void.
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
