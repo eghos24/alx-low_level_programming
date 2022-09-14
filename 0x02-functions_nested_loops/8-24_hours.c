d262169ec200d1dd119d7296dacb24a4ffbec066
Eghosa Igbinedion <eghos24@yahoo.com>
2022-09-13 21:39:50 -0700

    time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - entry point
+ * Description: print every second of a day
+ * Return: void
+ */
+void jack_bauer(void)
+{int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
+
+	for (hours_tens = 0; hours_tens <= 2 ; hours_tens++)
+	{
+		for (hours_ones = 0; hours_ones < 4; hours_ones++)
+		{
+			for (minutes_tens = 0; minutes_tens < 6; minutes_tens++)
+			{
+				for (minutes_ones = 0; minutes_ones < 10; minutes_ones++)
+				{
+					_putchar(hours_tens);
+					_putchar(hours_ones);
+					_putchar(':');
+					_putchar(minutes_tens);
+					_putchar(minutes_tens);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+	return (0);
+}
