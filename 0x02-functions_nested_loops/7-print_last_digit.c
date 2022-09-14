314ca0da9417b02b0fcde6188473c2983d812ca0
Eghosa Igbinedion <eghos24@yahoo.com>
2022-09-13 20:25:17 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ * Description: prints the absolite value of a given number
+ *@n: the integer value it recieves
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int i;
+
+	i = n % 10;
+	return (i);
+}
