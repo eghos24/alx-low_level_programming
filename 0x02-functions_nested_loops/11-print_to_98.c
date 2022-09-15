4dfec9196c4127cf5bc1ae2794304604e8155d7b
Eghosa Igbinedion <eghos24@yahoo.com>
2022-09-14 19:43:46 -0700

    time

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *add - start point
+ *@a: first input
+ *@b: second input
+ *Description : returns sum of the two number
+ *Return: answer
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,15 +9,15 @@ void jack_bauer(void)
 {
 	int hours_tens, hours_ones, minutes_tens, minutes_ones;
 
-	for (hours_tens = 0; hours_tens <= 2 ; hours_tens++)
+	for (hours_tens = 0; hours_tens <= 3 ; hours_tens++)
 	{
-		for (hours_ones = 0; hours_ones < 4; hours_ones++)
+		for (hours_ones = 0; hours_ones < 5; hours_ones++)
 		{
 			for (minutes_tens = 0; minutes_tens < 6; minutes_tens++)
 			{
 				for (minutes_ones = 0; minutes_ones < 10; minutes_ones++)
 				{
-					_putchar(hours_tens);
+					_putchar('hours_tens');
 					_putchar(hours_ones);
 					_putchar(':');
 					_putchar(minutes_tens);
