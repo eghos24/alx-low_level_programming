8af3973253336e9cba66974640903be9040472bf
Eghosa Igbinedion <eghos24@yahoo.com>
2022-09-14 20:38:36 -0700

    9times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,12 +9,27 @@ void times_table(void)
 {
 	int r;
        	int c;
+	int p;
+	int p_tens;
+	int p_ones;
+
 
 	for (r = '0'; r <= '9'; r++)
 	{
 		for (c = '0'; c <= '9'; c++)
-		{
-			_putchar(r*c);
+		{		
+			p = r * c;
+			if (p <= '9')
+			{
+				_putchar('p');
+			}
+			else
+			{
+				p_tens = p/10;
+				p_ones = p%10;
+				_putchar(p_tens);
+				_putchar(p_ones);
+			}	
 		}
 	}
 }
