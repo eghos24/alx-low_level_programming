eb961a1197124e0590af2cadebe2dd3d55c5b3f3
Eghosa Igbinedion <eghos24@yahoo.com>
2022-09-14 19:55:37 -0700

    time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,20 +10,19 @@ void jack_bauer(void)
 	int hours_tens, hours_ones, minutes_tens, minutes_ones;
 
 	for (hours_tens = '0'; hours_tens <= '3' ; hours_tens++)
-	{
-		_putchar(hours_tens); 
+	{ 
 		for (hours_ones = '0'; hours_ones < '5'; hours_ones++)
 		{
-			for (minutes_tens = 0; minutes_tens < 6; minutes_tens++)
+			for (minutes_tens = '0'; minutes_tens < '6'; minutes_tens++)
 			{
-				for (minutes_ones = 0; minutes_ones < 10; minutes_ones++)
+				for (minutes_ones = '0'; minutes_ones < '10'; minutes_ones++)
 				{
-					/*_putchar(hours_tens);
+					_putchar(hours_tens);
 					_putchar(hours_ones);
 					_putchar(':');
 					_putchar(minutes_tens);
 					_putchar(minutes_tens);
-					_putchar('\n');*/
+					_putchar('\n');
 				}
 			}
 		}
