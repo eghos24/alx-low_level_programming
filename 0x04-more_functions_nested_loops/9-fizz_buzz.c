5a43f14e82825288190d299daf1ccb948994c380
Eghosa Igbinedion <eghos24@yahoo.com>
2022-09-16 15:55:52 -0700

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - starting point
+ *
+ * Return: Alway success
+ */
+int main(void)
+{
+	int c = 1;
+
+	while (c <= 100)
+	{
+		if ((c % 3 == 0) && (c % 5 == 0))
+		{
+			printf("Fizz Buzz");
+		}
+		else if (c % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (c % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", c);
+		}
+		printf(" ");
+		c++;
+	}
+	printf("\n");
+	return (0);
+}
