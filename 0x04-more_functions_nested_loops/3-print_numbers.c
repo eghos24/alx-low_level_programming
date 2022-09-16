da10444671a79bddf19fcfc90b461360bd054af7
Eghosa Igbinedion <eghos24@yahoo.com>
2022-09-16 10:49:41 -0700

    print

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,11 @@
 void print_numbers(void)
 {
 	int c = 0;
-		
-		while (c <= 9)
-		{
-			_putchar(c + '0');
-			c++;
-		}
+
+	while (c <= 9)
+	{
+		_putchar(c + '0');
+		c++;
+	}
 	_putchar('\n');
 }
