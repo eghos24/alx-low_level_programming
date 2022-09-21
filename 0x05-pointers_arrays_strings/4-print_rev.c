5fd435a6facefd0479137c70ed294e1486bbd3b9
Eghosa Igbinedion <eghos24@yahoo.com>
2022-09-20 22:02:45 -0700

    m

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_rev - print 
+ * print_rev - print
  * @s: input string
  *
  * Return; Void
@@ -17,8 +17,7 @@ void print_rev(char *s)
 	while (j >= 0)
 	{
 		_putchar (s[j]);
-		j --;
+		j--;
 	}
-	
 	_putchar('\n');
 }
