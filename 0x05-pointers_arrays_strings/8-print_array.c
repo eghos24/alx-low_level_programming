4fa5420a3a0011ec3a429d6d4407bd17eab38d39
Eghosa Igbinedion <eghos24@yahoo.com>
2022-09-20 21:44:09 -0700

    all

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,12 +11,12 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if(i > 0)
+		if (i > 0)
 		{
 			printf(" ");
 		}
 		printf("%d", a[i]);
-		if(i < (n - 1))
+		if (i < (n - 1))
 		{
 			printf(",");
 		}
