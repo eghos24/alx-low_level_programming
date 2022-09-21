032ec9d91aa2fd08f74262149c278881393abcbd
Eghosa Igbinedion <eghos24@yahoo.com>
2022-09-20 21:52:04 -0700

    all

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,21 +1,23 @@
 #include "main.h"
 /**
  * print_rev - print 
- * @*s: input string
+ * @s: input string
  *
  * Return; Void
  */
 void print_rev(char *s)
 {
 	int tem;
+	int j;
 
 	for (tem = 0; s[tem] != '\0'; tem++)
 	{
 	}
+	j = tem - 1;
 	while (tem >= 0)
 	{
-		_putchar (s[tem]);
-		tem --;
+		_putchar (s[j]);
+		j --;
 	}
 	
 	_putchar('\n');
