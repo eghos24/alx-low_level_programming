1d4573e39a7acb9bfbdbfbdd6f696bcb13c2fb04
Eghosa Igbinedion <eghos24@yahoo.com>
2022-09-22 11:29:00 -0700

    6-cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,6 +10,10 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
+		if (str[0] >= 97 && str[0] <= 122)
+		{
+			str[0] = str[0] - 32;
+		}
 		if (str[i] == ' ' || str[i] == ('\t')
 				|| str[i] == '\n' || str[i] == ','
 				|| str[i] == ';' || str[i] == '.'
