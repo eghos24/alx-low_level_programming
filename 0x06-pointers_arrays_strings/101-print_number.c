782994e33e961568a15d1d09afc4cdf6408ac4db
Eghosa Igbinedion <eghos24@yahoo.com>
2022-09-22 21:38:28 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,14 +6,15 @@
  */
 void print_number(int n)
 {
+	unsigned int x;
+
+	x = n;
 	if (n < 0)
 	{
 		_putchar(45);
-		n = -n;
+		x = -n;
 	}
-	if (n / 10)
-	{
-		print_number(n / 10);
-	}
-	_putchar((n % 10) + '0');
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
