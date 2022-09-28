7d021d289951f486b4d725bb6dc4a599a9bdd8c9
Eghosa Igbinedion <eghos24@yahoo.com>
2022-09-27 21:11:59 -0700

    100

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 void print_chessboard(char (*a)[8]);
+void set_string(char **s, char *to);
 
 #endif /* MAIN_H */
