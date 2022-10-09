f93396cdfa6a2564c36615d21ce296c36c277e82
Eghosa Igbinedion <eghos24@yahoo.com>
2022-10-09 12:20:11 -0700

    grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ *alloc_grid - prints grid
+ *@width: input
+ *@height: input
+ *Return: NULL or Grid
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i;
+	int j;
+
+	grid = malloc(sizeof(int) * height);
+	if (width <= 0 || height <= 0 || grid == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < height; i++)
+		{
+			grid[i] = malloc(sizeof(int) * width);
+			if (grid[i] == 0)
+			{
+				free(grid);
+				return (NULL);
+			}
+			for (j = 0; j < width; j++)
+			{
+				grid[i][j] = 0;
+			}
+		}
+	}
+	return (grid);
+}
