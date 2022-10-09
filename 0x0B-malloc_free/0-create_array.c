8157aa2db067028e95c033310ad21a94edb86461
Eghosa Igbinedion <eghos24@yahoo.com>
2022-10-09 00:11:56 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *create_array - creates an array of chars
+ *@size: size of the array
+ *@c: the string
+ *Return: array or null
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *array;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	/*Define values with malloc*/
+	array = malloc(sizeof(char) * size);
+	while (i < size) /*while loop for create array*/
+	{
+		array[i] = c;
+		i++;
+	}
+	return (array);
+}
