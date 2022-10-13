91ee418a8882933601940cd5025a87faaf3676bc
Eghosa Igbinedion <eghos24@yahoo.com>
2022-10-12 19:20:09 -0700

    print_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints name
+ * @name: input
+ * @f: pointer
+ * Return: 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
