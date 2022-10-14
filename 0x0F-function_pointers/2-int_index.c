f824e1b0424516152fe3391ad439f507518b8b63
Eghosa Igbinedion <eghos24@yahoo.com>
2022-10-14 07:47:29 -0700

    third

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer
+ * @array: array
+ * @size: input size
+ * @cmp: input fucntion
+ * Return: -1(if no match)
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int ans;
+
+	for (i = 0; i < size; i++)
+	{
+		ans = cmp(array[i]);
+		if (ans == 1)
+		{
+			ans = i;
+			break;
+		}
+	}
+	return (ans);
+}
