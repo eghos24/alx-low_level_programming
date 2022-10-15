1d38ea516c49dee845e1339948c87632292d06d1
Eghosa Igbinedion <eghos24@yahoo.com>
2022-10-14 21:07:07 -0700

    last

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - add 2 num together
+ * @a: first input number
+ * @b: second input number
+ * Return: ans
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - sub 2 num together
+ * @a: first input number
+ * @b: second input number
+ * Return: ans
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - sub 2 num together
+ * @a: first input number
+ * @b: second input number
+ * Return: ans
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - sub 2 num together
+ * @a: first input number
+ * @b: second input number
+ * Return: ans
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod00 - sub 2 num together
+ * @a: first input number
+ * @b: second input number
+ * Return: ans
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
